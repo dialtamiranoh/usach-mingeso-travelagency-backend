2283e625d6f8aade8ffe507f81cbcdf27a86023b
Diego Altamirano <dialtamiranoh@gmail.com>
2026-05-01 16:57:25 -0400

    feat: initial spring boot project structure

---
+++ src/main/java/com/travelagency/travelagency_backend/TravelagencyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.travelagency.travelagency_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelagencyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelagencyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travelagency/travelagency_backend/TravelagencyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelagency.travelagency_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelagencyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
