0d63f145a4052af9eea2431dcbbd3cb85dc7f8f7
Diego Altamirano <dialtamiranoh@gmail.com>
2026-05-28 03:57:37 -0400

    fix: use MockitoBean and lenient stubbing to fix test failures

--- src/test/java/com/travelagency/travelagency_backend/TravelagencyBackendApplicationTests.java
+++ src/test/java/com/travelagency/travelagency_backend/TravelagencyBackendApplicationTests.java
@@ -2,12 +2,15 @@ package com.travelagency.travelagency_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class TravelagencyBackendApplicationTests {
 
+	@MockitoBean
+	private org.springframework.security.oauth2.jwt.JwtDecoder jwtDecoder;
+
 	@Test
 	void contextLoads() {
 	}
-
 }
--- src/test/java/com/travelagency/travelagency_backend/service/BookingServiceTest.java
+++ src/test/java/com/travelagency/travelagency_backend/service/BookingServiceTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.quality.Strictness;
+import org.mockito.junit.jupiter.MockitoSettings;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -22,6 +24,7 @@ import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class BookingServiceTest {
 
     @Mock private BookingRepository bookingRepository;
